e9f23528a6a04fef4fcc2e205690e42001ca82b3
EmpFams <siunidom2016.1@gmail.com>
2018-10-06 03:48:08 -0300

    farmacia

--- app/routes/home.js
+++
@@ -1,7 +0,0 @@
-//criacao de rotas para renderizacao de paginas atrvés da url
-
-module.exports = function(app){
-app.get('/', function(req,res){
-	res.render("home/index");
-	});
-}
--- mod_teste.js
+++
@@ -1,6 +0,0 @@
-
-//module.exports informa o que deve ser exportado
-module.exports = function(){
-	var msg = "Este modulo contem apenas uma string";
-	return msg;
-}; 
